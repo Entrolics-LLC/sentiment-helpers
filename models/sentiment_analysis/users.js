fc36a4f69b2ff1d990adb4367ad5c09bcc872e7f
m-yasir-khan <yasir.khan@mach3bi.com>
2022-10-27 13:20:25 +0500

    Update users.js

--- models/sentiment_analysis/users.js
+++ models/sentiment_analysis/users.js
@@ -42,7 +42,7 @@ module.exports = (sequelize, DataTypes) => {
         {
             sequelize,
             modelName: 'users',
-            schema: 'virgin_island',
+            schema: 'sentiment_analysis',
             createdAt: false,
             updatedAt: false
         });
