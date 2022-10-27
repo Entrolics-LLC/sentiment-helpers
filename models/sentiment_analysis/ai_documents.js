768505d8faedf98239e8af1f9c59c0ad59884ddd
syedfakher27 <sfakher27@gmail.com>
2022-10-27 14:57:06 +0500

    Ai_docments table added

---
+++ models/sentiment_analysis/ai_documents.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+    Model, NOW
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Ai_Document extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Ai_Document.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            document_id: DataTypes.STRING,
+            topics: DataTypes.ARRAY(DataTypes.STRING),
+            redacted_comment: DataTypes.STRING(10000),
+            additional_fields:DataTypes.JSON,
+            created_at: {
+                type: DataTypes.DATE
+            },
+            is_deleted:{ 
+                type:DataTypes.BOOLEAN,
+                defaultValue: false
+            }
+        },
+        {
+            sequelize,
+            modelName: 'ai_documents',
+            schema: 'sentiment_analysis',
+            createdAt: false,
+            updatedAt: false
+        });
+    return Ai_Document;
+};
